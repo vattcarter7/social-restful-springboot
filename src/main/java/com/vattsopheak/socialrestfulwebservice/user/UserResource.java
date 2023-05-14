69377dba0e98ce3f741dd1e3ea98c08728fde416
Vatt Sopheak <vattcarter@gmail.com>
2023-05-14 13:09:11 +0700

    Added validation

--- src/main/java/com/vattsopheak/socialrestfulwebservice/user/UserResource.java
+++ src/main/java/com/vattsopheak/socialrestfulwebservice/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.vattsopheak.socialrestfulwebservice.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -39,7 +40,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         // /users/4 => /users , user.getID
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
