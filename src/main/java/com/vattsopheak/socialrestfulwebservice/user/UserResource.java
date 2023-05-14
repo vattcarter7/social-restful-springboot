f56be1fb1a4d741db88c1d3e4f27d6f78c3cd225
Vatt Sopheak <vattcarter@gmail.com>
2023-05-14 12:28:18 +0700

    Implement Generic Exception Handling

--- src/main/java/com/vattsopheak/socialrestfulwebservice/user/UserResource.java
+++ src/main/java/com/vattsopheak/socialrestfulwebservice/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.vattsopheak.socialrestfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,12 +23,24 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id:"+id);
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
-
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        // /users/4 => /users , user.getID
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
