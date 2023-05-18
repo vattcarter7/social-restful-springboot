eab5256a3e2cdf534596906dd637a0849796e182
Vatt Sopheak <vattcarter@gmail.com>
2023-05-18 19:27:54 +0700

    Implemented Static Filtering

--- src/main/java/com/vattsopheak/socialrestfulwebservice/user/User.java
+++ src/main/java/com/vattsopheak/socialrestfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.vattsopheak.socialrestfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,7 +9,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
+
+    @JsonProperty("birth_date")
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
